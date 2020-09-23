7ef96a792d68cc037dc2051a2ce3a1e450bc7ce9
Eric Watkins <eric@threehour.co>
2020-09-23 14:57:21 -0500

    Something

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 /**
  * 
  * hello
- * The main template file.
+ * This is the main template file.
+ * 
  *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
